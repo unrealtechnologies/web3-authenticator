4897714182d02a88b713edf1699c91682e4a5617
Ariel Saldana <ariel@unreal.io>
2022-11-21 21:34:23 -0500

    Use httpclient test class from httpservice

--- http-client/build.gradle.kts
+++ http-client/build.gradle.kts
@@ -5,14 +5,3 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.web3j:core:5.0.0")
 }
-
-//tasks.withType<KotlinCompile> {
-//    kotlinOptions {
-//        freeCompilerArgs = listOf("-Xjsr305=strict")
-//        jvmTarget = "17"
-//    }
-//}
-
-//tasks.withType<Test> {
-//    useJUnitPlatform()
-//}
--- http-service/src/main/kotlin/io/unreal/web3authenticator/controllers/Authenticator.kt
+++ http-service/src/main/kotlin/io/unreal/web3authenticator/controllers/Authenticator.kt
@@ -3,11 +3,14 @@ package io.unreal.web3authenticator.controllers
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
+import io.unreal.web3authenticator.httpclient.Test
+
 @RestController
 class Authenticator {
 
     @GetMapping("/authenticate")
     fun authenticate(): String {
-        return "Simple authenticate endpoint"
+        val test = Test()
+        return test.log()
     }
 }
