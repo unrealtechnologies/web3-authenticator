eb4607952ef5d0331a4c33d03db3904a9eac902c
Ariel Saldana <ariel@unreal.io>
2022-11-29 16:35:31 -0500

    progress

--- http-service/src/main/kotlin/io/unreal/web3authenticator/services/AuthenticatorService.kt
+++ http-service/src/main/kotlin/io/unreal/web3authenticator/services/AuthenticatorService.kt
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service
 @Service
 class AuthenticatorService {
     fun getBlockByHash(): InfuraResponseBody {
-        val infuraApi = InfuraApi("f4a33b53695443739067c53f6b89a5e2")
+        val infuraApi = InfuraApi("")
         return infuraApi.blockApi.getBlockByHash("0xb3b20624f8f0f86eb50dd04688409e5cea4bd02d700bf6e79e9384d47d6a5a35", false)
     }
 
