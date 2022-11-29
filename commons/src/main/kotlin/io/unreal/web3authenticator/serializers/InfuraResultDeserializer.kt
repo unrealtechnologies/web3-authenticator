2279bbac837cd70c9e1939776e0ed1090c110f15
Ariel Saldana <ariel@unreal.io>
2022-11-29 16:35:05 -0500

    commit progress

---
+++ commons/src/main/kotlin/io/unreal/web3authenticator/serializers/InfuraResultDeserializer.kt
@@ -0,0 +1,6 @@
+package io.unreal.web3authenticator.serializers
+
+import com.fasterxml.jackson.databind.JsonDeserializer
+import com.fasterxml.jackson.databind.KeyDeserializer
+
+//class InfuraResultDeserializer: JsonDeserializer
