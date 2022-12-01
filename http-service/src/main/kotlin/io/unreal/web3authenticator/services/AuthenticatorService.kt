24c501e9de45d86f5602facbda3154ce22bafa32
Ariel Saldana <ariel@unreal.io>
2022-11-30 22:00:20 -0500

    Update latestBlock endpoint (#7)
    * Upgrade the latestBlock API Endpoint to include blockNumber and blockHash, fix the getBlockByHash function in the service to allow resolving a passed value.
    
    * spacing

--- http-service/src/main/kotlin/io/unreal/web3authenticator/services/AuthenticatorService.kt
+++ http-service/src/main/kotlin/io/unreal/web3authenticator/services/AuthenticatorService.kt
@@ -1,14 +1,18 @@
 package io.unreal.web3authenticator.services
 
+import io.unreal.web3authenticator.commons.objects.BlockHash
+import io.unreal.web3authenticator.commons.objects.BlockInformation
 import io.unreal.web3authenticator.commons.objects.InfuraResponseBody
 import io.unreal.web3authenticator.httpclient.ethereum.infura.InfuraApi
+import io.unreal.web3authenticator.objects.BlockObject
 import org.springframework.stereotype.Service
 
 @Service
 class AuthenticatorService {
     val infuraApi = InfuraApi("")
-    fun getBlockByHash(): InfuraResponseBody {
-        return infuraApi.blockApi.getBlockByHash("0xb3b20624f8f0f86eb50dd04688409e5cea4bd02d700bf6e79e9384d47d6a5a35", false)
+
+    fun getBlockByHash(hash: String): InfuraResponseBody {
+        return infuraApi.blockApi.getBlockByHash(hash, false)
     }
 
     fun getLatestBlockHash(): InfuraResponseBody {
@@ -17,6 +21,16 @@ class AuthenticatorService {
 
     fun getBlockByBlockNumber(blockNumber: String): InfuraResponseBody {
         return infuraApi.blockApi.getBlockInformationWithBlockBumber(blockNumber, false)
+    }
+
+    fun getLatestBlockHashAndBlockNumber(): BlockObject {
+        val latestBlockNumber = getLatestBlockHash().result as BlockHash
+        val latestBlockInformation = getBlockByBlockNumber(latestBlockNumber.toString()).result as BlockInformation
+
+        return BlockObject(
+            blockNumber = latestBlockNumber.toString(),
+            blockHash = latestBlockInformation.hash
+        )
     }
 
     fun verifyBlockHash(): Boolean {
